c4e253dad176403066fb39e619244fd4eac39d01
Guiadan <guyadam3@gmail.com>
2018-12-16 14:25:24 +0200

    run experiments script

--- baselines/experiments.py
+++ baselines/experiments.py
@@ -1,10 +1,7 @@
 import os
 
 for i in range(5):
-	try:
-        args = "-m baselines.run --alg=deepq --env=EnduroNoFrameskip-v4 --num_timesteps=1e6 --seed={}".format(i)
-        print('run #{}'.format(i+1))
-        print('python ' + args)
-        os.system('python ' + args)
-    except KeyboardInterrupt:
-        exit(0)
+	args = "-m baselines.run --alg=deepq --env=EnduroNoFrameskip-v4 --num_timesteps=1e4 --seed={}".format(i)
+	print('run #{}'.format(i+1))
+	print('python ' + args)
+	os.system('python ' + args)
