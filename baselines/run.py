6880ec3fe87747b9e25480fa0635bc177affe6bd
Guiadan <guyadam3@gmail.com>
2018-12-16 14:03:11 +0200

    bug fixes

--- baselines/run.py
+++ baselines/run.py
@@ -188,48 +188,49 @@ def parse_cmdline_kwargs(args):
 
 
 
-def main(args):
+def main(args, seed=0):
     # configure logger, disable logging in child MPI processes (with rank > 0)
     blr_param = BLRParams()
     arg_parser = common_arg_parser()
     args, unknown_args = arg_parser.parse_known_args(args)
     extra_args = parse_cmdline_kwargs(unknown_args)
-    for s in range(5):
-        args.seed = s
-        if MPI is None or MPI.COMM_WORLD.Get_rank() == 0:
-            rank = 0
-            logger.configure(dir='./baselines/deepq/exp/{}_seed_{}_blr_batch_{}_no_prior'.format(
-                            args.env,args.seed,blr_param.batch_size))
-        else:
-            logger.configure(format_strs=[])
-            rank = MPI.COMM_WORLD.Get_rank()
+    args.seed = seed
+    if MPI is None or MPI.COMM_WORLD.Get_rank() == 0:
+        rank = 0
+        logger.configure(dir='./baselines/deepq/exp/{}_seed_{}_blr_batch_{}_no_prior'.format(
+                        args.env,args.seed,blr_param.batch_size))
+    else:
+        logger.configure(format_strs=[])
+        rank = MPI.COMM_WORLD.Get_rank()
 
-        model, env = train(args, extra_args)
+    model, env = train(args, extra_args)
+    env.close()
+
+    if args.save_path is not None and rank == 0:
+        save_path = osp.expanduser(args.save_path)
+        model.save(save_path)
+
+    if args.play:
+        logger.log("Running trained model")
+        env = build_env(args)
+        obs = env.reset()
+        def initialize_placeholders(nlstm=128,**kwargs):
+            return np.zeros((args.num_env or 1, 2*nlstm)), np.zeros((1))
+        state, dones = initialize_placeholders(**extra_args)
+        while True:
+            actions, _, state, _ = model.step(obs,S=state, M=dones)
+            obs, _, done, _ = env.step(actions)
+            env.render()
+            done = done.any() if isinstance(done, np.ndarray) else done
+
+            if done:
+                obs = env.reset()
+
         env.close()
-
-        if args.save_path is not None and rank == 0:
-            save_path = osp.expanduser(args.save_path)
-            model.save(save_path)
-
-        if args.play:
-            logger.log("Running trained model")
-            env = build_env(args)
-            obs = env.reset()
-            def initialize_placeholders(nlstm=128,**kwargs):
-                return np.zeros((args.num_env or 1, 2*nlstm)), np.zeros((1))
-            state, dones = initialize_placeholders(**extra_args)
-            while True:
-                actions, _, state, _ = model.step(obs,S=state, M=dones)
-                obs, _, done, _ = env.step(actions)
-                env.render()
-                done = done.any() if isinstance(done, np.ndarray) else done
-
-                if done:
-                    obs = env.reset()
-
-            env.close()
 
     return model
 
 if __name__ == '__main__':
-    main(sys.argv)
+    for s in range(5):
+        print("seed is now: {}".format(s))
+        main(sys.argv, seed=s)
