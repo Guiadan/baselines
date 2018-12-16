f7f9a91fe948af9eb47c1e10d77afb1d564167dd
Guiadan <guyadam3@gmail.com>
2018-12-16 14:30:10 +0200

    run experiments script

--- baselines/experiments.py
+++ baselines/experiments.py
@@ -1,7 +1,7 @@
 import os
 
 for i in range(5):
-	args = "-m baselines.run --alg=deepq --env=EnduroNoFrameskip-v4 --num_timesteps=1e4 --seed={}".format(i)
+	args = "-m baselines.run --alg=deepq --env=EnduroNoFrameskip-v4 --num_timesteps=1e6 --seed={}".format(i)
 	print('run #{}'.format(i+1))
 	print('python ' + args)
 	os.system('python ' + args)
