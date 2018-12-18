a02d2768eec9736de6aba83c7d05086594f4b274
Guiadan <guyadam3@gmail.com>
2018-12-18 22:21:00 +0200

    bug fixes

--- baselines/experiments.py
+++ baselines/experiments.py
@@ -3,9 +3,9 @@ import sys
 
 for i in range(5):
 	try:
-       args = "-m baselines.run --alg=deepq --env=EnduroNoFrameskip-v4 --num_timesteps=1e6 --seed={}".format(i)
-       print('run #{}'.format(i+1))
-       print('python ' + args)
-       os.system('python ' + args)
+        args = "-m baselines.run --alg=deepq --env=EnduroNoFrameskip-v4 --num_timesteps=1e6 --seed={}".format(i)
+        print('run #{}'.format(i+1))
+        print('python ' + args)
+        os.system('python ' + args)
     except KeyboardInterrupt:
-       sys.exit()
+        sys.exit()
