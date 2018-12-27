20bee388e200b22587abec62aa7275ec64e0408b
Ubuntu <ubuntu@ip-172-31-18-26.eu-west-1.compute.internal>
2018-12-27 11:21:18 +0000

    dqn experiments

--- baselines/experiments.py
+++ baselines/experiments.py
@@ -3,13 +3,13 @@ import sys
 from datetime import datetime
 
 fmt = '%Y-%m-%d %H:%M:%S'
-ddqn =[False, False, True, True]
-exp_name = ['nl_no_prior', 'nl_simple_prior', 'nl_no_prior_ddqn', 'nl_simple_ddqn']
-prior = ['no prior', 'simple', 'no prior', 'simple']
+ddqn =[False]
+exp_name = ['dqn']
+prior = ['no prior']
 for i in range(1):
-    for env in ['EnduroNoFrameskip-v4', 'AmidarNoFrameskip-v4', 'BankHeistNoFrameskip-v4', 'AsterixNoFrameskip-v4', 'AtlantisNoFrameskip-v4']:
+    for env in ['AsterixNoFrameskip-v4']:
         for seed in range(5):
-            args = "-m baselines.run --alg=deepq --env={} --num_timesteps=1e7 --seed={} --neural_linear=True --ddqn={}" \
+            args = "-m baselines.run --alg=deepq --env={} --num_timesteps=5e7 --seed={} --neural_linear=False --ddqn={}" \
                    " --exp_name={} --prior={}".format(env, seed, ddqn[i], exp_name[i],prior[i])
             print('run #{}'.format(seed+1))
             print('python ' + args)
